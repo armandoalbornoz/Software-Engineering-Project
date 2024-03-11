820456933ee751d0bc7f3270f2cb72670e3d343c
Armando Albornoz <albornoz.armando31416@gmail.com>
2024-03-10 20:38:32 -0400

    =Starting frontend, added a json server, and basic components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,30 @@
-import Message from './Message'
+import './Navbar'
+import Navbar from './Navbar';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Create from './Create';
+import UserMedInfo from './UserMedInfo';
+import NotFound from './NotFound';
+
 
 function App() {
-  return <div><Message></Message></div>
+  const title = "Welcome to the app"
+  return (
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/signup" element={<Create/>} />
+            <Route path="/medical-information/:id" element={<UserMedInfo/>} />
+            <Route path="*" element={<NotFound/>} />
+
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/UserMedInfo.js
@@ -0,0 +1,56 @@
+import { useNavigate, useParams } from "react-router-dom";
+import useFetchGet from "../customHooks/useGetFetch";
+import { useState } from "react";
+
+
+const UserMedInfo = () => {
+    const {id} = useParams() //Grabs the url param
+    const {data, error, isPending} = useFetchGet(`http://localhost:8000/medicalInfo/${id}`)
+    const  [deleteErr, setDeleteErr]  = useState("");
+    const navigate = useNavigate()
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/medicalInfo/' + id,{
+            method: 'DELETE'
+        })
+        .then((res) => {
+            if (!res.ok)
+            {
+                console.log(res);
+                throw Error("Could not delete the data for that resource.")
+            }
+            navigate('/')
+        })
+        .catch((err) => {
+            setDeleteErr(err.message)
+        })
+    }
+
+    return ( 
+        <div className="medical-info-details">
+            { isPending && <div>Loading...</div> }
+            { error && <div>{error}</div> }
+            { deleteErr && <div>{deleteErr}</div> }
+
+
+            { data && (
+                <article> 
+                    <h2>Medical Information of {data.firstName} {data.lastName} </h2>
+                    <div>
+                       <p> Message: {data.message}</p>
+                       <p> Height: {data.height}</p>
+                       <p> Weight: {data.weight}</p>  
+                       <p> BMI: {data.BMI}</p>    
+  
+                    </div>
+                   
+                    <button onClick={handleDelete}> delete </button>
+                </article>
+            )}
+
+
+        </div>
+     );
+}
+ 
+export default UserMedInfo;
